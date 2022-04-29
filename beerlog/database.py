7d49378b7c68e801959704943a94afabc9df15e5
Ricardo Pegoraro <ricardo.pegoraro@gmail.com>
2022-04-29 08:12:26 -0300

    immplemented cli

--- beerlog/database.py
+++ beerlog/database.py
@@ -1,4 +1,12 @@
-from sqlmodel import create_engine
+import warnings
+from sqlalchemy.exc import SAWarning
+from sqlmodel.sql.expression import Select, SelectOfScalar
+
+warnings.filterwarnings("ignore", category=SAWarning)
+SelectOfScalar.inherit_cache = True
+Select.inherit_cache = True
+
+from sqlmodel import create_engine, Session
 from beerlog.config import settings
 from beerlog import models
 
@@ -6,3 +14,5 @@ engine = create_engine(settings.database.url)
 
 models.SQLModel.metadata.create_all(engine)
 
+def get_session():
+    return Session(engine)
--- beerlog/models.py
+++ beerlog/models.py
@@ -22,6 +22,6 @@ class Beer(SQLModel, table=True):
         return v
 
     @validator("rate", always=True)
-    def calculate_rate(cls, v, values): 
+    def calculate_rate(cls, v, values):
         rate = mean([values["flavor"], values["image"], values["cost"]])
         return int(rate)
